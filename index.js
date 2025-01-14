7d4df4d865764e9d9ec3b3a622f13fc08352c64e
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-01-15 04:51:29 +0530

    Basic express server setup with cors,express,dotenv,mongoDB

---
+++ index.js
@@ -0,0 +1,38 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from CampFlow Server..");
+});
+
+app.listen(port, () => {
+  console.log(`plantNet is running on port ${port}`);
+});
